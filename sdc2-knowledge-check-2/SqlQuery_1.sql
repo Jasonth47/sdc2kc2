d5a2f03b1661ca90ac2af1f83190d42cfdd4865f
Jasonth47 <71046222+Jasonth47@users.noreply.github.com>
2023-11-01 14:23:53 -0400

    Add files via upload

---
+++ sdc2-knowledge-check-2/SqlQuery_1.sql
@@ -0,0 +1,8 @@
+﻿SELECT *
+FROM Customers
+WHERE CustomerID = (SELECT CustomerID FROM Orders WHERE OrderID = 10310);
+
+
+SELECT *
+FROM Suppliers
+WHERE SupplierID = (SELECT SupplierID FROM Products WHERE ProductID = 40);
